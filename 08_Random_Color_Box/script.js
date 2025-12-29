768816c778ccffb51d16cc5878a4d93a828835b1
abdul09651-developer <abdul09651@gmail.com>
2025-12-29 14:54:03 +0530

    Generating random color boxes using DOM concept

--- 08_Random_Color_Box/script.js
+++ 08_Random_Color_Box/script.js
@@ -1,39 +1,18 @@
 
-let boxes = document.getElementsByClassName("box")
+// let boxes = document.getElementsByClassName("box")
+let boxes = document.querySelector(".container").children
 
-function Box1() {
-    let r = Math.floor(Math.random()*254);
-    let b = Math.floor(Math.random()*254);
-    let g = Math.floor(Math.random()*254);
+function getRandomColor() {
+    let r = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
     
-    boxes[0].style.backgroundColor=`rgb(${r},${b},${g})`;
+    return `rgb(${r},${b},${g})`
 }
-Box1()
 
-function Box2() {
-    let r = Math.floor(Math.random()*254);
-    let b = Math.floor(Math.random()*254);
-    let g = Math.floor(Math.random()*254);
-    
-    boxes[1].style.backgroundColor=`rgb(${r},${b},${g})`;
-}
-Box2()
-
-function Box3() {
-    let r = Math.floor(Math.random()*254);
-    let b = Math.floor(Math.random()*254);
-    let g = Math.floor(Math.random()*254);
-    
-    boxes[2].style.backgroundColor=`rgb(${r},${b},${g})`;
-}
-Box3()
-
-function Box4() {
-    let r = Math.floor(Math.random()*254);
-    let b = Math.floor(Math.random()*254);
-    let g = Math.floor(Math.random()*254);
-    
-    boxes[3].style.backgroundColor=`rgb(${r},${b},${g})`;
-}
-Box4()
+Array.from(boxes).forEach(e=>{
+    // console.log(e)
+    e.style.backgroundColor=getRandomColor()
+    e.style.color=getRandomColor()
+})
 
