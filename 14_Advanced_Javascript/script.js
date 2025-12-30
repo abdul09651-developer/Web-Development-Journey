eaf0937654081f602b54d4f26807a9a5a873b547
abdul09651-developer <abdul09651@gmail.com>
2025-12-30 19:03:51 +0530

    Advanced Javascript Concept Completed

---
+++ 14_Advanced_Javascript/script.js
@@ -0,0 +1,34 @@
+async function sleep() {
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=> {
+            resolve(45)
+        },1000);
+    })
+}
+
+function sum(a,b,c){
+return a + b + c
+}
+//IIFE = Immediately Invoke Function Expression
+(async function main() {
+    // let a = await sleep()
+    // console.log(a)
+    // let b = await sleep()
+    // console.log(b)
+
+    // let [x,y, ...rest] = [1,5,7,8,9,10]
+    // console.log(x , y, rest)
+
+    const obj ={
+        a:1,
+        b:2,
+        c:3
+    }
+    const {a,b} = obj
+    console.log(a,b)
+
+    let arr = [1,4,3]
+    console.log(sum(arr[0],arr[1],arr[2]))
+    console.log(sum(...arr))//spread operator
+
+})()
